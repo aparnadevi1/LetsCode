4f27e39df0c69434ec7cea11ce7fbc0ac5e0cd45
aparnadevigm <aparnach1269@gmail.com>
2024-07-13 02:14:52 +0530

    Adding error handing middleware with basic error classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,28 +1,31 @@
 const { StatusCodes } = require("http-status-codes");
+const Notimplemented = require("../errors/notimplemented.error");
 
 function addProblem(req, res) {
   return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-    message: "Not Implemented",
+    message: "Not emented",
   });
 }
 function getProblem(req, res) {
   return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-    message: "Not Implemented",
+    message: "Not Impleme",
   });
 }
-function getProblems(req, res) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-    message: "Not Implemented",
-  });
+function getProblems(req, res, next) {
+  try {
+    throw new Notimplemented("addProblem");
+  } catch (error) {
+    next(error);
+  }
 }
 function deleteProblem(req, res) {
   return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-    message: "Not Implemented",
+    message: " Implemented",
   });
 }
 function updateProblem(req, res) {
   return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-    message: "Not Implemented",
+    message: "Not ted",
   });
 }
 function pingProblemController(req, res) {
